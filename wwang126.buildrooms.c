4c2a3d6ae5fb1a884f46f486070044ece87e4e92
wwang126 <wavelet.wang@gmail.com>
2017-10-15 00:59:13 -0700

    added extra includes just incase

--- wwang126.buildrooms.c
+++ wwang126.buildrooms.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
 //Room struct that defines the room
 struct room {
-    int id; //Room ID 
+    int id; //Room ID
     int roomType;//Type of room
     char *name;//Name of room
     int connectOut;//Number of outbound connections
