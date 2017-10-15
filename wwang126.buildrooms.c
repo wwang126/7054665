7ca6260d32f41866de743aa21ac89998fd55f60d
wwang126 <wavelet.wang@gmail.com>
2017-10-15 01:03:11 -0700

    Changed formatting spaces -> tabs

--- wwang126.buildrooms.c
+++ wwang126.buildrooms.c
@@ -16,61 +16,54 @@ struct room {
 };
 
 // Create all connections in graph
-while (IsGraphFull() == false)
-{
-  AddRandomConnection();
+while (IsGraphFull() == false) {
+    AddRandomConnection();
 }
 
 // Returns true if all rooms have 3 to 6 outbound connections, false otherwise
-bool IsGraphFull()
-{
-  ...
+bool IsGraphFull() {
+    //TODO
 }
 
 // Adds a random, valid outbound connection from a Room to another Room
-void AddRandomConnection()
-{
-  Room A;  //Maybe a struct, maybe global arrays of ints
-  Room B;
+void AddRandomConnection() {
+    Room A;  //Maybe a struct, maybe global arrays of ints
+    Room B;
 
-  while(true)
-  {
+    while(true)
+    {
     A = GetRandomRoom();
 
     if (CanAddConnectionFrom(A) == true)
-      break;
-  }
+        break;
+    }
 
-  do
-  {
+    do
+    {
     B = GetRandomRoom();
-  }
-  while(CanAddConnectionFrom(B) == false || IsSameRoom(A, B) == true);
+    }
+    while(CanAddConnectionFrom(B) == false || IsSameRoom(A, B) == true);
 
-  ConnectRoom(A, B);
-  ConnectRoom(B, A);
+    ConnectRoom(A, B);
+    ConnectRoom(B, A);
 }
 
 // Returns a random Room, does NOT validate if connection can be added
-Room GetRandomRoom()
-{
-  ...
+Room GetRandomRoom() {
+    //TODO
 }
 
 // Returns true if a connection can be added from Room x, false otherwise
-bool CanAddConnectionFrom(Room x)
-{
-  ...
+bool CanAddConnectionFrom(Room x) {
+    //TODO
 }
 
 // Connects Rooms x and y together, does not check if this connection is valid
-void ConnectRoom(Room x, Room y)
-{
-  ...
+void ConnectRoom(Room x, Room y) {
+    //TODO
 }
 
 // Returns true if Rooms x and y are the same Room, false otherwise
-bool IsSameRoom(Room x, Room y)
-{
-  ...
+bool IsSameRoom(Room x, Room y) {
+    //TODO
 }
